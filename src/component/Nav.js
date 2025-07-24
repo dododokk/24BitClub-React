e0b8a39fe78af015f0809f8ff0e85ea1375bfa8b
d0d0d0d0k <kdoeun05@konkuk.ac.kr>
2025-07-25 00:14:09 +0900

    board

---
+++ src/component/Nav.js
@@ -0,0 +1,45 @@
+// Nav.js
+import React, { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { AuthContext } from '../context/AuthContext';
+import "../style/Nav.css";
+
+function Nav() {
+  const navigate = useNavigate();
+  const { isLoggedIn, setIsLoggedIn } = useContext(AuthContext);
+
+  const handleLogin = () => {
+    navigate('/login');
+  };
+
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+    navigate('/');
+  };
+
+  const handleMyPage = () => {
+    navigate('/mypage');
+  };
+
+  const handleRegister = () => {
+    navigate('/register');
+  };
+
+  return (
+    <nav>
+      {isLoggedIn ? (
+        <>
+          <button onClick={handleLogout}>로그아웃</button>
+          <button onClick={handleMyPage}>마이페이지</button>
+        </>
+      ) : (
+        <>
+          <button onClick={handleLogin}>로그인</button>
+          <button onClick={handleRegister}>회원가입</button>
+        </>
+      )}
+    </nav>
+  );
+}
+
+export default Nav;
