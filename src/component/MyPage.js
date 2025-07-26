052122d7e932e591ba519ac21972dd70857dc726
d0d0d0d0k <kdoeun05@konkuk.ac.kr>
2025-07-26 18:03:54 +0900

    마이페이지 menu

--- src/component/MyPage.js
+++ src/component/MyPage.js
@@ -1,20 +1,44 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { UserContext } from "../context/UserContext";
 import profile from "../image/profile.png";
 import setting from "../image/setting.png";
 import "../style/MyPage.css"
 
-function MyPage(){
-    const {userId, setUserId} = useContext(UserContext);
+function Label(props) {
+    return (
+        <label className={`menu-item ${props.selected === props.menu ? "active" : ""}`}
+            onClick={() => props.onSelect(props.menu)}>
+            {props.title}
+        </label>
+    )
+}
 
-    return(
-        <div id="myInfo">
-            <img src={profile} id="profile"/>
-            <span id="id">
-                <span id="lv">Lv.1</span><br />
-                <span id="idName">{userId}</span>
-            </span>
-            <span id="setting"><img src={setting} id="set" /></span>
+function MyPage() {
+    const { userId, setUserId } = useContext(UserContext);
+    const [selectedMenu, setSelectedMenu] = useState("menu1");
+
+    return (
+        <div id="mypage">
+            <div id="myInfo">
+                <img src={profile} id="profile" />
+                <span id="id">
+                    <span id="lv">Lv.1</span><br />
+                    <span id="idName">{userId}</span>
+                </span>
+                <span id="setting"><img src={setting} id="set" /></span>
+            </div>
+            <div id="mywrite-content">
+                <div id="mywrite">
+                    <Label selected={selectedMenu} menu="menu1" onSelect={setSelectedMenu} title="내 게시물" />
+                    <Label selected={selectedMenu} menu="menu2" onSelect={setSelectedMenu} title="댓글 단 게시물" />
+                    <Label selected={selectedMenu} menu="menu3" onSelect={setSelectedMenu} title="좋아요 누른 게시물" />
+                </div>
+                <article>
+                    {selectedMenu === "menu1" && <p>menu1</p>}
+                    {selectedMenu === "menu2" && <p>menu2</p>}
+                    {selectedMenu === "menu3" && <p>menu3</p>}
+                </article>
+            </div>
         </div>
     );
 }
