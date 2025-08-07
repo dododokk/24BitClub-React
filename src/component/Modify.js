4023bd995b0a1d4d89fe3b388974b703052a0356
d0d0d0d0k <kdoeun05@konkuk.ac.kr>
2025-08-07 21:36:39 +0900

    수정 페이지 및 디자인 수정

--- src/component/Modify.js
+++ src/component/Modify.js
@@ -1,24 +1,30 @@
 import { useLocation, useNavigate } from "react-router-dom";
 import styles from "../style/Modify.module.css";
-import { useEffect, useState, useRef} from "react";
+import { useEffect, useState, useRef } from "react";
 
 function Modify() {
     const location = useLocation();
     // 현재 페이지의 URL 정보와 함께 이동할 때 전달된 데이터(state)를 읽을 수 있게 해줌.
     const navigate = useNavigate();
     const inputRef = useRef(null);
-    const {post} = location.state || {};
+    const { post } = location.state || {};
     const [currentPost, setCurrentPost] = useState(post || {});
     // const { id, userId } = location.state || {};
 
     const [newTitle, setNewTitle] = useState("");
     const [newContent, setNewContent] = useState("");
 
-    useEffect(()=>{
-            if(post){
-                setCurrentPost(post);
+    useEffect(() => {
+        if (post) {
+            setCurrentPost(post);
+            setNewTitle(currentPost.title);
+            setNewContent(currentPost.content);
+
+            if (inputRef.current) {
+                inputRef.current.innerHTML = post.content;
             }
-        }, [post]);
+        }
+    }, [post]);
     // useEffect(() => {
     //     if (id && userId) {
     //         //id랑 userId 넘기고 해당 포스트 내용 받기.
@@ -56,10 +62,37 @@ function Modify() {
     //     }
     // },[]);
 
-    const handleSubmit = ()=>{
+    const handleSubmit = async () => {
         setNewContent(inputRef.current.innerHTML);
-        //서버로 수정 요청 보내기. id, userId, newTitle, newContent 활용
-        navigate("/mypage");
+        const postId = currentPost.id;
+        const userId = currentPost.user.id;
+
+        // try {
+        //     const response = await fetch(
+        //         `http://localhost:3000/api/posts/${postId}?userId=${userId}&title=${encodeURIComponent(newTitle)}`,
+        //         {
+        //             method: "PUT",
+        //             headers: {
+        //                 "Content-Type": "application/json"
+        //             },
+        //             body: JSON.stringify({
+        //                 content: newContent
+        //             })
+        //         }
+        //     );
+
+        //     if (!response.ok) {
+        //         throw new Error("게시글 수정 실패");
+        //     }
+
+        //     const result = await response.json();
+        //     console.log("수정 성공:", result);
+
+        //     navigate("/mypage");
+        // }catch(err){
+        //     console.error("에러 발생:",err);
+        // }
+        // 서버로 수정 요청 보내기
     };
 
     const applyStyle = (command, value = null) => {
@@ -74,27 +107,27 @@ function Modify() {
 
                 <label for="title">제목</label>
                 <input type="text" name="title" value={newTitle}
-                onChange={(e)=>setNewTitle(e.target.value)}/>
+                    onChange={(e) => setNewTitle(e.target.value)} />
                 <label for="content">내용</label>
                 <div className={styles['editor-toolbar']}>
-                    <select onChange={(e)=>applyStyle('fontSize', e.target.value)}>
+                    <select onChange={(e) => applyStyle('fontSize', e.target.value)}>
                         <option value="" disabled hidden selected>글자 크기</option>
                         <option value="1">작게</option>
                         <option value="3">보통</option>
                         <option value="5">크게</option>
                     </select>
                     <button onClick={() => applyStyle('bold')}><b>B</b></button>
-                    <button onClick={()=>applyStyle('italic')}><i>I</i></button>
-                    <button onClick={()=>applyStyle('underline')}><u>U</u></button>
+                    <button onClick={() => applyStyle('italic')}><i>I</i></button>
+                    <button onClick={() => applyStyle('underline')}><u>U</u></button>
                 </div>
                 <div
-                ref={inputRef}
+                    ref={inputRef}
                     id={styles.content}
                     contentEditable
                     suppressContentEditableWarning={true}
-                    // onInput={(e)=>setNewContent(e.currentTarget.innerHTML)}
-                    dangerouslySetInnerHTML={{__html: newContent}}
-                    ></div>
+                    onInput={(e) => setNewContent(e.currentTarget.innerHTML)}
+                // dangerouslySetInnerHTML={{__html: newContent}}
+                ></div>
                 {/* <textarea id={styles.content} name="content" value={newContent}
                 onChange={(e)=>setNewContent(e.target.value)} /> */}
                 <div className={styles['btn-container']}>
